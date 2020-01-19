538d3b51a12e6ff7a625be3d31020a9d9609f24c
Bee Flaherty <beeflaherty@gmail.com>
2020-01-19 20:58:48 +0000

    Sort the tasks.

--- src/js/store.js
+++ src/js/store.js
@@ -6,7 +6,7 @@ export default {
 	},
 	mutations: {
 		loadTasks: function(state, tasks) {
-            state.tasks = tasks;
+            state.tasks = tasks.sort((a, b) => parseFloat(a.importance) - parseFloat(b.importance));;
         },
         deleteTaskFromList: function(state, task) {
             for (let i = 0; i < state.tasks.length; i++) {
